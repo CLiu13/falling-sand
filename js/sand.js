5abf42d2010a86606a4fa3ca240ba2a123f1967f
takore05 <60117058+takore05@users.noreply.github.com>
2020-07-20 21:48:48 -0400

    integrate water and sand particles with website

--- js/sand.js
+++ js/sand.js
@@ -1,3 +1,111 @@
+//class for individual water droplets
+class Droplet {
+  //creates droplet
+  constructor(xval, yval) {
+    //creates x,y, and diameter properties of droplet
+    //also creates speed property of how fast the droplets move back and forth 
+    this.x = xval;
+    this.y = yval;
+    this.diameter = 2;
+    this.speed = 1;
+  }
+
+  run() {
+    this.fall();
+    this.display();
+  }
+
+  fall() {
+    if (get(this.x,this.y+1)[0]==255 && get(this.x,this.y+1)[1]==255 && get(this.x,this.y+1)[2]==255) {
+      //only falls if pixel below is empty/not white
+      this.y += 2;
+    }
+    else{
+        this.move();
+    }
+  }
+  atBottom(){
+   if(this.y>height-16){
+     return false;
+   }
+    return true;
+  }
+  
+  move(){
+      this.x += random(-this.speed, this.speed);//moves droplet back and forth at random speed
+
+  }
+  
+
+  display() {
+    stroke(0,220,220);
+    fill(0, 180,225);
+    circle(this.x, this.y, this.diameter);
+  }
+  
+
+}
+
+//class for group of water particles
+class DropSystem{
+  constructor(){
+    this.dropsystem = [];
+    this.length =0;
+  }
+  
+  addNewDrop(drop){
+    this.dropsystem[this.length] = drop;
+    this.length++;
+  }
+  
+  checkDrops(x,y){
+    for(let i=0;i<this.length;i++){
+      if(x===this.dropsystem[this.length].x && y===this.dropsystem[this.length].y){
+      return true;
+      }
+    }
+    return false;
+  }
+  
+  collisionCheck(){
+    for(let i=0;i<this.length;i++){ //goes through array of droplets
+      if(this.dropsystem[i].atBottom() ){ 
+      let angle=0.0;
+      while(angle<360){
+        let colx = 1*cos(angle) + this.dropsystem[i].x;
+        let coly = 1*sin(angle) + this.dropsystem[i].y; //checks the points along the circumference of the droplet to see if there is another object
+        let bounces = false;
+        if(get(colx,coly)[0]!=255 && get(colx,coly)[1]!=255 && get(colx,coly)[2]!=255 ){
+        for(let j=0;j<this.length;j++){
+            //checks to see if the droplet is colliding with another droplet  
+            if(j!=i && colx!=this.dropsystem[i].x){
+              bounces = true;
+            }
+            if(j!=i && coly==this.dropsystem[i].y){
+              bounces = true;
+          }
+        }
+      }
+        if(bounces){ //if the droplet is colliding with another object adds the difference to its x and y
+          this.dropsystem[i].x += this.dropsystem[i].x - colx;
+          this.dropsystem[i].y += this.dropsystem[i].y - coly;
+        }
+        angle+=30.0;
+        //goes through the circumference at 30 degree intervals to prevent lagging 
+      }
+    }
+    }
+    
+  }
+  
+  runDrops(){
+    this.collisionCheck();
+    for (let j = 0; j < this.length; j++) {
+      this.dropsystem[j].run();
+    }
+  }
+}
+//class for individual sand particles
 class SandParticle {
   //creates particle
   constructor(xval, yval) {
@@ -62,7 +170,7 @@ class SandParticle {
   }
 }
 
-
+//class for rectangle
 class Rectangle {
   constructor(x, y, w, h, r){
     this.x = x;
@@ -110,15 +218,15 @@ let rectangles = [new Rectangle(200, 50, 200, 5, 1/4),
                  new Rectangle(400, 300, 200, 5, -1/4)];
 
 let i = 0;
+let system = new DropSystem();
+let material =1;
 
 function draw() {
   background('white');
-  
-  
   //creates bottom 'floor' of structure
   push();
-  stroke("green");
-  strokeWeight(25);
+  stroke("black");
+  strokeWeight(15);
   line(0,selector.clientHeight-5, selector.clientWidth, selector.clientHeight-5);
   pop();
   
@@ -138,13 +246,34 @@ function draw() {
   for (let j = 0; j < i; j++) {
     sand[j].run();
   }
+  system.runDrops();
 
-  // create sand particle
-  if (mouseIsPressed) {
-    sand[i] = new SandParticle(mouseX, mouseY);
-    i++;
+  if(mouseIsPressed){
+    if(material===1){
+      sand[i] = new SandParticle(mouseX, mouseY);
+      i++;
+    }
+    if(material===2){
+      system.addNewDrop(new Droplet(random(mouseX - 20, mouseX + 20), random(mouseY - 20, mouseY + 20)) );
+    }
+
   }
+
+  
+
+
 }
+
+function differentMaterial(check){
+  if(check===1){
+    material=1;
+  }
+  if(check===2){
+    material=2;
+  }
+
+}
+
 
 function collided(x, y){
   for(var i = 0; i < rectangles.length; i++){
@@ -162,3 +291,4 @@ function collided(x, y){
   
   return 0;
 }
+
--- js/water.js
+++ js/water.js
@@ -2,10 +2,11 @@ class Droplet {
   //creates droplet
   constructor(xval, yval) {
     //creates x,y, and diameter properties of droplet
+    //also creates speed property of how fast the droplets move back and forth 
     this.x = xval;
     this.y = yval;
-    this.diameter = 1;
-    this.speed = 1
+    this.diameter = 2;
+    this.speed = 1;
   }
 
   run() {
@@ -14,32 +15,33 @@ class Droplet {
   }
 
   fall() {
-
-
-    if (this.y < height-16 ) {
-
+    if (get(this.x,this.y+1)[0]==255 && get(this.x,this.y+1)[1]==255 && get(this.x,this.y+1)[2]==255) {
+      //only falls if pixel below is empty/not white
       this.y += 2;
-
-
     }
     else{
-        this.y -=1;
-
+        this.move();
     }
-    this.move();
+  }
+  atBottom(){
+   if(this.y>height-16){
+     return false;
+   }
+    return true;
+  }
+  
+  move(){
+      this.x += random(-this.speed, this.speed);//moves droplet back and forth at random speed
 
   }
-  move(){
-    this.x += random(-this.speed, this.speed);
-  }
+  
 
   display() {
-    stroke(0,150,220);
+    stroke(0,220,220);
+    fill(0, 180,225);
     circle(this.x, this.y, this.diameter);
   }
-  getY(){
-    return(this.y);
-  }
+  
 
 }
 
@@ -51,30 +53,75 @@ class DropSystem{
     this.dropsystem = [];
     this.length =0;
   }
-
+  
   addNewDrop(drop){
     this.dropsystem[this.length] = drop;
     this.length++;
   }
-
+  
+  checkDrops(x,y){
+    for(let i=0;i<this.length;i++){
+      if(x===this.dropsystem[this.length].x && y===this.dropsystem[this.length].y){
+      return true;
+      }
+    }
+    return false;
+  }
+  
+  collisionCheck(){
+    for(let i=0;i<this.length;i++){ //goes through array of droplets
+      if(this.dropsystem[i].atBottom() ){ 
+      let angle=0.0;
+      while(angle<360){
+        let colx = 1*cos(angle) + this.dropsystem[i].x;
+        let coly = 1*sin(angle) + this.dropsystem[i].y; //checks the points along the circumference of the droplet to see if there is another object
+        let bounces = false;
+        if(get(colx,coly)[0]!=255 && get(colx,coly)[1]!=255 && get(colx,coly)[2]!=255 ){
+        for(let j=0;j<this.length;j++){
+            //checks to see if the droplet is colliding with another droplet  
+            if(j!=i && colx!=this.dropsystem[i].x){
+              bounces = true;
+            }
+            if(j!=i && coly==this.dropsystem[i].y){
+              bounces = true;
+          }
+        }
+      }
+        if(bounces){ //if the droplet is colliding with another object adds the difference to its x and y
+          this.dropsystem[i].x += this.dropsystem[i].x - colx;
+          this.dropsystem[i].y += this.dropsystem[i].y - coly;
+        }
+        angle+=30.0;
+        //goes through the circumference at 30 degree intervals to prevent lagging 
+      }
+    }
+    }
+    
+  }
+  
   runDrops(){
+    this.collisionCheck();
     for (let j = 0; j < this.length; j++) {
       this.dropsystem[j].run();
     }
   }
-  checkOverlap(){
-   /* let t=this.length;
-    let i=0;
-    while(t>-1){
-      if(this.dropsystem[t].y === this.dropsystem[i].y){
-        print("overlap");
-      }
-      t--;
-      } */
-    }
-
-
 }
+
+class Ground{
+  constructor(height, width){
+    this.h=height;
+    this.w=width;
+  }
+  display(){
+    push()
+    stroke("black");
+    strokeWeight(15);
+    line(0,this.h, this.w, this.h);
+    pop()
+  }
+}
+
+
 
 let system = new DropSystem();
 
@@ -86,30 +133,27 @@ function setup() {
 function draw() {
   background('white');
   //creates new background on every draw to create falling
-
+  
   //creates bottom 'floor' of structure
-  push()
-  stroke("green");
+  let ground = new Ground(height,width);
+  ground.display();
+  
+  push();
+  stroke("black");
   strokeWeight(15);
-  line(0,height, width, height);
-  pop()
+  line(600,200, 200,500);
+  pop();
 
   system.runDrops();
+  
 
-  if(system.length>1){
-    system.checkOverlap();
-  }
+  waterParticles();
+}
 
-
-
-  //when the mouse is pressed is calls the function to create a droplet
+function waterParticles(){
   if (mouseIsPressed === true) {
-      system.addNewDrop(new Droplet(random(mouseX - 20, mouseX + 20), random(mouseY - 20, mouseY + 20)) ); //droplet is created anywhere 20 pixels around the circle
-    //changes drops position and displays
-
+      system.addNewDrop(new Droplet(random(mouseX - 20, mouseX + 20), random(mouseY - 20, mouseY + 20)) ); 
+    
   }
-
-
-
 
 }
