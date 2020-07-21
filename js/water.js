f3aac40bcd83335a98d1c96a8a044900619e45f0
takore05 <60117058+takore05@users.noreply.github.com>
2020-07-21 14:21:48 -0400

    new changes to organization of js files and edited collisionCheck function to reduce lag

--- js/water.js
+++ js/water.js
@@ -6,7 +6,7 @@ class Droplet {
     this.x = xval;
     this.y = yval;
     this.diameter = 2;
-    this.speed = 1;
+    this.speed = 2;
   }
 
   run() {
@@ -17,19 +17,12 @@ class Droplet {
   fall() {
     if (get(this.x,this.y+1)[0]==255 && get(this.x,this.y+1)[1]==255 && get(this.x,this.y+1)[2]==255) {
       //only falls if pixel below is empty/not white
-      this.y += 2;
+      this.y += 1;
     }
     else{
         this.move();
     }
   }
-  atBottom(){
-   if(this.y>height-16){
-     return false;
-   }
-    return true;
-  }
-  
   move(){
       this.x += random(-this.speed, this.speed);//moves droplet back and forth at random speed
 
@@ -69,30 +62,23 @@ class DropSystem{
   }
   
   collisionCheck(){
-    for(let i=0;i<this.length;i++){ //goes through array of droplets
-      if(this.dropsystem[i].atBottom() ){ 
+    for(let i=0;i<this.length;i++){
+      if(this.dropsystem[i].y<height-17){
       let angle=0.0;
       while(angle<360){
         let colx = 1*cos(angle) + this.dropsystem[i].x;
-        let coly = 1*sin(angle) + this.dropsystem[i].y; //checks the points along the circumference of the droplet to see if there is another object
+        let coly = 1*sin(angle) + this.dropsystem[i].y;
         let bounces = false;
-        if(get(colx,coly)[0]!=255 && get(colx,coly)[1]!=255 && get(colx,coly)[2]!=255 ){
-        for(let j=0;j<this.length;j++){
-            //checks to see if the droplet is colliding with another droplet  
-            if(j!=i && colx!=this.dropsystem[i].x){
+        if(get(colx,coly)[0]!=255 && get(colx,coly)[1]!=255 && get(colx,coly)[2]!=255  ){
+            if(get(colx,coly)[0]!=0 && get(colx,coly)[1]!=180 && get(colx,coly)[2]!=225){
               bounces = true;
             }
-            if(j!=i && coly==this.dropsystem[i].y){
-              bounces = true;
-          }
-        }
       }
-        if(bounces){ //if the droplet is colliding with another object adds the difference to its x and y
+        if(bounces){
           this.dropsystem[i].x += this.dropsystem[i].x - colx;
           this.dropsystem[i].y += this.dropsystem[i].y - coly;
         }
         angle+=30.0;
-        //goes through the circumference at 30 degree intervals to prevent lagging 
       }
     }
     }
@@ -122,38 +108,63 @@ class Ground{
 }
 
 
-
-let system = new DropSystem();
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  selector = document.getElementById("particle-canvas");
+  canvas = createCanvas(selector.clientWidth, selector.offsetHeight+100);
+  
+  canvas.parent("particle-canvas");
 }
 
+function windowResized(){
+  selector = document.getElementById("particle-canvas");
+  canvas = createCanvas(selector.clientWidth, selector.offsetHeight);
+  canvas.parent("particle-canvas");
+}
 
 function draw() {
   background('white');
-  //creates new background on every draw to create falling
-  
   //creates bottom 'floor' of structure
-  let ground = new Ground(height,width);
-  ground.display();
-  
   push();
   stroke("black");
   strokeWeight(15);
-  line(600,200, 200,500);
+  line(0,selector.clientHeight-5, selector.clientWidth, selector.clientHeight-5);
   pop();
-
-  system.runDrops();
   
+  rectangles.forEach((r)=>{
+    push();
+    translate(r.x, r.y);
+    rotate(r.rot)
+    stroke(0);
+    fill(0);
 
-  waterParticles();
-}
+    rect(0,0, r.w, r.h);
 
-function waterParticles(){
-  if (mouseIsPressed === true) {
-      system.addNewDrop(new Droplet(random(mouseX - 20, mouseX + 20), random(mouseY - 20, mouseY + 20)) ); 
+    pop();
+  });
+  
+  //changes drops position and displays 
+  for (let j = 0; j < i; j++) {
+    sand[j].run();
+  }
+  system.runDrops();
+
+  if(mouseIsPressed){
+    if(material===1){
+      sand[i] = new SandParticle(mouseX, mouseY);
+      i++;
+    }
+    if(material===2){
+      system.addNewDrop(new Droplet(random(mouseX - 20, mouseX + 20), random(mouseY - 20, mouseY + 20)) );
+    }
     
+
   }
 
+  
+
+
+}
+
+function differentMaterial(check){
+  material=check;
 }
