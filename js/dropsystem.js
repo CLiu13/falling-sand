baacb91dd3a3ca83374d7b9a80a8c41c8ed7d0e6
takore05 <60117058+takore05@users.noreply.github.com>
2020-07-27 15:46:56 -0400

    update dropsystem.js

--- js/dropsystem.js
+++ js/dropsystem.js
@@ -1,6 +1,7 @@
 class ParticleManager {
     constructor() {
         this.particles = [];
+        this.waterparticles = [];
         // 1 for sand, 2 for water
         this.material = 1;
         this.rectangles = [new Rectangle(200, 50, 200, 5, 1/4),
@@ -11,12 +12,14 @@ class ParticleManager {
         let obj;
 
         if(this.material == 1) obj = new SandParticle(random(x - 10, x + 10), random(y - 10, y + 10));
-        else if (this.material == 2) obj = new WaterParticle(random(x - 10, x + 10), random(y - 10, y + 10));
+        else if (this.material == 2){ obj = new WaterParticle(random(x - 10, x + 10), random(y - 10, y + 10));
+        this.waterparticles.push(obj);}
 
         this.particles.push(obj);
     }
 
     simulate() {
+      this.collisionCheck();
         for (let i = 0; i < this.particles.length; i++) {
             this.particles[i].run();
             this.particles[i].display();
@@ -26,6 +29,37 @@ class ParticleManager {
     differentMaterial(check) {
         this.material = check;
     }
+
+    collisionCheck(){
+    for(let i=0;i<this.waterparticles.length;i++){ //goes through array of droplets
+      if(this.waterparticles[i].atBottom() ){ 
+      let angle=0.0;
+      while(angle<360){
+        let colx = 1*cos(angle) + this.waterparticles[i].x;
+        let coly = 1*sin(angle) + this.waterparticles[i].y; //checks the points along the circumference of the droplet to see if there is another object
+        let bounces = false;
+        if(get(colx,coly)[0]!=255 && get(colx,coly)[1]!=255 && get(colx,coly)[2]!=255 ){
+        for(let j=0;j<this.length;j++){
+            //checks to see if the droplet is colliding with another droplet  
+            if(j!=i && colx!=this.waterparticles[i].x){
+              bounces = true;
+            }
+            if(j!=i && coly==this.waterparticles[i].y){
+              bounces = true;
+          }
+        }
+      }
+        if(bounces){ //if the droplet is colliding with another object adds the difference to its x and y
+          this.waterparticles[i].x += this.waterparticles[i].x - colx;
+          this.waterparticles[i].y += this.waterparticles[i].y - coly;
+        }
+        angle+=90.0;
+        //goes through the circumference at 30 degree intervals to prevent lagging 
+      }
+    }
+    }
+    
+  }
 
     collided(x, y) {
         for (var i = 0; i < this.rectangles.length; i++) {
